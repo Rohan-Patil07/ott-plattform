453b706210d65ef69a9c856e81f8cc16ebc6748d
Rohan <rohanpatil9529@gmail.com>
2021-10-01 13:05:22 +0530

    my project

---
+++ ott/static/js/viewvideos.js
@@ -0,0 +1,27 @@
+// alert("upload")
+getVideos()
+function getVideos(){
+    let formData = new FormData();
+    formData.append("txtID",$("#txtID").val());
+    formData.append(
+      "csrfmiddlewaretoken",
+      $("input[name=csrfmiddlewaretoken]").val()
+    );
+    
+
+    $.ajax({
+        url: "/get_view_videos/",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response){
+        let video = response[0].v_video.substring(3,)
+          $("#playvideo").append('<source src="'+video+'" type="video/mp4">')
+          $("#video_title").html(response[0].v_title)
+          $("#desc").text(response[0].v_desc)
+          $('#Genre').text("Genre : " + response[0].v_cat)
+        }
+        
+        });
+}
